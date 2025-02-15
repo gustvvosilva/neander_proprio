4ee644ad8e757b0068284948cd70337097bf0fb6
gustvvosilva <gustavosilvadeoliveira02@gmail.com>
2025-02-15 01:50:27 -0300

    multiplicacao e soma de numeros negativos e positivos funcionando.

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 
 int main(){
 
-    FILE *file = fopen("soma.mem", "rb");
+    FILE *file = fopen("mult_positivo.mem", "rb");
     
     __uint8_t bytes[256];
     __uint8_t byte[1];
     int i2 = 0;
 
     __uint8_t ac = 0, pc = 0;
-    bool neg = false, zer = false;
+    bool neg, zer;
 
     for(int i = 0; i < 516; i++) {
 
@@ -26,11 +26,30 @@ int main(){
     }
     fclose(file);
 
-    // bytes[0x80] = 0x04;
-    // bytes[0x81] = 0xFE;
+    // Mude esse valores para trocar os caracteres da operação.
+    // bytes[0x80] = 0xF7;
+    // bytes[0x81] = 0xFB;
 
     while(bytes[pc] != 0xf0) {
 
+        if(ac >= 0x80)
+            neg = true;
+        else
+            neg = false;
+
+        if(ac == 0)
+            zer = true;
+        else
+            zer = false;
+
+        printf("AC:%02x PC:%02x %s %s", ac, pc, neg?"neg":"pos", zer?"zer":"nze");
+        for(int i = 0; i < 256; i++){
+            if(i % 16 == 0)
+                printf("\n");
+            printf("%02x ", bytes[i]);
+        }
+        printf("\n===================================================\n");
+            
         switch(bytes[pc]) {
 
             case 0x10:
@@ -47,18 +66,40 @@ int main(){
             pc++;
             ac += bytes[bytes[pc]];
             break;
+
+            case 0x60:
+            ac = ~ac;
+            break;
+
+            case 0x80:
+            pc++;
+            pc = bytes[pc] - 0x01;
+            break;
+
+            case 0x90:
+            pc++;
+            if(neg) {
+                pc = bytes[pc] - 0x01;
+            }
+            break;
+
+            case 0xa0:
+            pc++;
+            if(zer) {
+                pc = bytes[pc] - 0x01;
+            }
+            break;
         }
 
-        printf("AC:%02x PC:%02x", ac, pc);
+        pc++;
+    }
+    printf("AC:%02x PC:%02x %s %s", ac, pc, neg?"neg":"pos", zer?"zer":"nze");
         for(int i = 0; i < 256; i++){
             if(i % 16 == 0)
                 printf("\n");
             printf("%02x ", bytes[i]);
         }
         printf("\n===================================================\n");
-
-        pc++;
-    }
     printf("cabo\n");
 
     return 0;
